5776022b7ead15e82f7e7619781cab34a57b8aad
River Phillips <riverphillips1@gmail.com>
2020-01-04 11:45:24 +0000

    Implement domain layer
    Signed-off-by: River Phillips <riverphillips1@gmail.com>

---
+++ domain/domain.go
@@ -0,0 +1,60 @@
+package domain
+
+import (
+	"errors"
+)
+
+type CustomerRepository interface {
+	Store(customer Customer)
+	FindById(id int) Customer
+}
+
+type ItemRepository interface {
+	Store(item Item)
+	FindById(id int) Item
+}
+
+type OrderRepository interface {
+	Store(order Order)
+	FindById(id int) Order
+}
+
+type Customer struct {
+	Id   int
+	Name string
+}
+
+type Item struct {
+	Id        int
+	Name      string
+	Value     float64
+	Available bool
+}
+
+type Order struct {
+	Id       int
+	Customer Customer
+	Items    []Item
+}
+
+func (order *Order) Add(item Item) error {
+	if !item.Available {
+		return errors.New("Cannot add unavailable items to order")
+	}
+
+	if order.value()+item.Value > 250.00 {
+		return errors.New("An order cannot exceed a ttotal value of £250")
+	}
+
+	order.Items = append(order.Items, item)
+	return nil
+}
+
+func (order *Order) value() float64 {
+	sum := 0.0
+	for i := range order.Items {
+		sum = sum + order.Items[i].Value
+	}
+
+	return sum
+}
